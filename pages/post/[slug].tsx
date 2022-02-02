b3d9be204f5d0391a8337a8878d5272623060f2f
Benndora1 <Benndora2@gmail.com>
2022-02-02 14:27:53 +0300

    post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,28 @@
-
+import { CountQueuingStrategy } from "stream/web";
+import Header from "../../components/Header";
+import { sanityClient, urlFor} from "../../sanity";
 function Post() {
-  return <div></div>;
+  return <main>
+<Header/>
+  </main>;
 }
+
 export default Post;
+
+
+export const getStaticPaths = async () => {
+     const query = `*[type == "post"]{
+         _id,
+         slug{ 
+             current
+         }
+     };`
+
+     const post = await sanityClient.fetch(query); 
+
+     const paths = posts.map((post: Post) => ({
+         params: {
+             slug: post.slug.current
+         }
+     })) 
+};
